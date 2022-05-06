820d6b721700829067dd4ffd771f0da1fc6742fc
EC2 Default User <ec2-user@ip-172-31-12-100.ec2.internal>
2022-05-06 04:21:06 +0000

    20220506　8章まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  get 'top' => 'homes#top'
+  resources :lists
+  
+  #get 'lists/new'
+  #get 'top' => 'home#top'
+  #post 'lists' => 'lists#create'
+  #get 'lists' => 'lists#index'
+  #get 'lists/:id' => 'lists#show', as: 'list'
+  #get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
